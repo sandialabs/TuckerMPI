a1a4f3f791e591cc2a0c52471b92feb49354eef8
Zitong Li <liz20@pegasus.deac.wfu.edu>
2020-07-14 21:14:54 -0400

    minor changes to make compatible with Cluster environment

--- src/mpi/drivers/TuckerMPI_LQ.cpp
+++ src/mpi/drivers/TuckerMPI_LQ.cpp
@@ -51,11 +51,10 @@ int main(int argc, char* argv[])
   Tucker::Timer unpack_timer;
   Tucker::Timer localqr_dcopy_timer;
   Tucker::Timer localqr_decompose_timer;
-  double localqr_decompose_time;
   Tucker::Timer localqr_transpose_timer;
   if(rank == 0) total_timer.start();
   Tucker::Matrix* L0 = TuckerMPI::LQ(&X, 0, &tsqr_timer, &local_qr_timer, &pack_timer, &alltoall_timer, &unpack_timer,
-    &localqr_dcopy_timer, &localqr_decompose_timer, &localqr_decompose_time, &localqr_transpose_timer);
+    &localqr_dcopy_timer, &localqr_decompose_timer, &localqr_transpose_timer);
   if(rank == 0){
     total_timer.stop();
     std::cout << "total time used: " << total_timer.duration()
@@ -64,8 +63,7 @@ int main(int argc, char* argv[])
       << "\n unpack time: " << unpack_timer.duration()
       << "\n local qr time: " << local_qr_timer.duration() 
       << "\n local qr dcopy time: " << localqr_dcopy_timer.duration()
-      //<< "\n local qr decompose time: " << localqr_decompose_timer.duration()
-      << "\n local qr decompose time: " << localqr_decompose_time
+      << "\n local qr decompose time: " << localqr_decompose_timer.duration()
       << "\n local qr transpose time: " << localqr_transpose_timer.duration()
       << "\n tsqr time: " << tsqr_timer.duration() << std::endl;
   }
