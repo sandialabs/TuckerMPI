85993d73bb72c0d540859ebec0cb83adfb871d46
antoinemeyer5 <antoine.meyer54@gmail.com>
2023-05-10 15:39:26 +0200

    Init branch with new ttm kokkosblas implement

--- src/kokkosonly/ttm.hpp
+++ src/kokkosonly/ttm.hpp
@@ -3,7 +3,128 @@
 
 #include "Tucker_BlasWrapper.hpp"
 
+#include<Kokkos_Core.hpp>
+#include<KokkosBlas3_gemm.hpp>
+
 namespace TuckerKokkos{
+
+template <class ScalarType, class MemorySpace>
+void ttm_kokkosblas_impl(const Tensor<ScalarType, MemorySpace>* const X,
+        const int n,
+        const Kokkos::View<ScalarType**, Kokkos::LayoutLeft, MemorySpace> A,
+	      const int strideU,
+	      Tensor<ScalarType, MemorySpace> & Y,
+	      bool Utransp)
+{
+  // Check that the input is valid
+  //assert(Uptr != 0);
+  //assert(Y != 0);
+  assert(n >= 0 && n < X->N());
+  for(int i=0; i<X->N(); i++) {
+    if(i != n) {
+      assert(X->size(i) == Y.size(i));
+    }
+  }
+
+  auto X_view_d = X->data();
+  auto X_ptr_d = X_view_d.data();
+
+  auto Y_view_d = Y.data();
+  auto Y_ptr_d = Y_view_d.data();
+
+  // n = 0 is a special case
+  // Y_0 is stored column major
+  if(n == 0)
+  {
+    // Compute number of columns of Y_n
+    // Technically, we could divide the total number of entries by n,
+    // but that seems like a bad decision
+    size_t ncols = X->size().prod(1,X->N()-1);
+
+    
+
+    if(ncols > std::numeric_limits<int>::max()) {
+      std::ostringstream oss;
+      oss << "Error in Tucker::ttm: " << ncols
+          << " is larger than std::numeric_limits<int>::max() ("
+          << std::numeric_limits<int>::max() << ")";
+      throw std::runtime_error(oss.str());
+    }
+
+    // Call matrix matrix multiply
+    // call gemm (TRANSA, TRANSB, M, N, K, ALPHA, A, LDA, B, LDB, BETA, C, LDC)
+    // C := alpha*op( A )*op( B ) + beta*C
+    // A, B and C are matrices, with op( A ) an m by k matrix,
+    // op( B ) a k by n matrix and C an m by n matrix.
+    char transa;
+    const char transb = 'N';
+    const ScalarType alpha = ScalarType(1);
+    const ScalarType beta = ScalarType(0);
+
+    if(Utransp) {
+      transa = 'T';
+    } else {
+      transa = 'N';
+    }
+
+    int M = 2;
+    int N = 4;
+
+    Kokkos::View<ScalarType**, Kokkos::LayoutLeft, Kokkos::MemoryTraits<Kokkos::Unmanaged>> B(X_ptr_d, N, 16);
+    // 1d view, unchange on exit
+    Kokkos::View<ScalarType**, Kokkos::LayoutLeft, Kokkos::MemoryTraits<Kokkos::Unmanaged>> C(Y_ptr_d, 2, 16);
+    // 1d view, overwritten by the M by N matrix ( alpha*op( A )*op( B ) + beta*C ).
+    
+    KokkosBlas::gemm(&transa,&transb,alpha,A,B,beta,C); // need 2d view with 1d
+
+  }
+  else
+  {
+    // Count the number of columns
+    size_t ncols = X->size().prod(0,n-1);
+
+    // Count the number of matrices
+    size_t nmats = X->size().prod(n+1,X->N()-1,1);
+
+    if(ncols > std::numeric_limits<int>::max()) {
+      std::ostringstream oss;
+      oss << "Error in Tucker::ttm: " << ncols
+          << " is larger than std::numeric_limits<int>::max() ("
+          << std::numeric_limits<int>::max() << ")";
+      throw std::runtime_error(oss.str());
+    }
+
+    // For each matrix...
+    for(size_t i=0; i<nmats; i++) {
+      // Call matrix matrix multiply
+      // call dgemm (TRANSA, TRANSB, M, N, K, ALPHA, A, LDA, B, LDB, BETA, C, LDC)
+      // C := alpha*op( A )*op( B ) + beta*C
+      // A, B and C are matrices, with op( A ) an m by k matrix,
+      // op( B ) a k by n matrix and C an m by n matrix.
+      char transa = 'N';
+      char transb;
+      int m = (int)ncols;
+      int blas_n = Y.size(n);
+      int lda = (int)ncols;
+      int ldb = strideU;
+      int ldc = (int)ncols;
+      ScalarType alpha = 1;
+      ScalarType beta = 0;
+      if(Utransp) {
+        transb = 'N';
+      } else {
+        transb = 'T';
+      }
+      /*
+      Tucker::gemm(&transa, &transb, &m, &blas_n, &k, &alpha,
+		   X_ptr_h+i*k*m, &lda, Uptr, &ldb, &beta,
+		   Y_ptr_h+i*m*blas_n, &ldc);
+      */
+      // TODO HERE
+    }
+  }
+
+}
 
 template <class ScalarType, class MemorySpace>
 void ttm_impl(const Tensor<ScalarType, MemorySpace>* const X,
