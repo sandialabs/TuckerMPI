c5a59de77b48e5f96d005213b61c91f9d250212e
antoinemeyer5 <antoine.meyer54@gmail.com>
2023-06-08 09:48:02 +0200

    Remove class from driver

--- src/kokkos/mpi-distributed/drivers/TuckerMpi_sthosvd.cpp
+++ src/kokkos/mpi-distributed/drivers/TuckerMpi_sthosvd.cpp
@@ -1,6 +1,5 @@
 #include "Tucker_CmdLineParse.hpp"
 #include "TuckerMpi_ParameterFileParser.hpp"
-#include "TuckerMpi_Distribution.hpp"
 
 #include <mpi.h>
 #include <Kokkos_Core.hpp>
@@ -27,9 +26,6 @@ int main(int argc, char* argv[])
     if(rank == 0) {
       inputs.describe();
     }
-
-    // set up distribution object
-    TuckerMpiDistributed::Distribution dist(inputs.sizeArrayOfDataTensor(), inputs.proc_grid_dims);
 
     // TO CONTINUE
     // SEE mpi AND kokkosonly CODES
