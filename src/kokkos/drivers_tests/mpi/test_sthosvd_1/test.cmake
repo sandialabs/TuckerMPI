4dd5ed1d6786de01f5772c55b833bc52bcd3e316
Francesco Rizzi <fnrizzi@sandia.gov>
2023-06-20 11:01:53 +0200

    add test stuff

---
+++ src/kokkos/drivers_tests/mpi/test_sthosvd_1/test.cmake
@@ -0,0 +1,18 @@
+include(FindUnixCommands)
+
+set(CMD "${MPIEXEC} -n ${NUMPROC} ${EXENAME}")
+message(STATUS ${CMD})
+execute_process(COMMAND ${BASH} -c ${CMD} RESULT_VARIABLE RES)
+if(RES)
+  message(FATAL_ERROR "run failed")
+else()
+  message("run succeeded!")
+endif()
+
+set(CMD "python3 compare.py")
+execute_process(COMMAND ${BASH} -c ${CMD} RESULT_VARIABLE RES)
+if(RES)
+  message(FATAL_ERROR "comparison failed")
+else()
+  message("comparison succeeded!")
+endif()
