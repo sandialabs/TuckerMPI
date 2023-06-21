c68936544ad8626c5ffdd9de32aa1082b4ed0d1b
Francesco Rizzi <fnrizzi@sandia.gov>
2023-06-21 17:32:33 +0200

    generate binary file as part of the test for portability

--- src/kokkos/drivers_tests/mpi/test_sthosvd_1/test.cmake
+++ src/kokkos/drivers_tests/mpi/test_sthosvd_1/test.cmake
@@ -1,6 +1,13 @@
 include(FindUnixCommands)
 
-# put here command to run script taking txt file and dumping binary file
+# run script taking txt file and dumping binary file
+set(CMD "python3 ascii_to_binary_tensor_data.py -i ${TENSORASCIIDATAFILE} -o ${TENSORBINDATAFILE} --skip ${SKIPROWS}")
+execute_process(COMMAND ${BASH} -c ${CMD} RESULT_VARIABLE RES)
+if(RES)
+  message(FATAL_ERROR "binary convertion failed")
+else()
+  message("comparison succeeded!")
+endif()
 
 set(CMD "${MPIEXEC} -n ${NUMPROC} ${EXENAME}")
 message(STATUS ${CMD})
