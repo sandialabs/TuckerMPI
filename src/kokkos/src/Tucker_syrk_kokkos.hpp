5256b176e7546fded240feed0c13a0b8472b90cf
Francesco Rizzi <fnrizzi@sandia.gov>
2023-07-04 08:19:50 +0200

    modularizing syrk

---
+++ src/kokkos/src/Tucker_syrk_kokkos.hpp
@@ -0,0 +1,121 @@
+
+#ifndef TUCKER_SYRK_HPP_
+#define TUCKER_SYRK_HPP_
+
+#include <Kokkos_Core.hpp>
+#include <Kokkos_StdAlgorithms.hpp>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+
+namespace Tucker{
+namespace impl{
+
+template <class AlphaType, class BetaType, class AViewType, class CViewType>
+struct SyrkFunctor1
+{
+  AlphaType alpha_;
+  BetaType beta_;
+  AViewType Aview_;
+  CViewType Cview_;
+  SyrkFunctor1(AViewType Av, CViewType Cv,
+	       AlphaType alpha, BetaType beta)
+    : Aview_(Av), Cview_(Cv), alpha_(alpha), beta_(beta){}
+
+  KOKKOS_FUNCTION void operator()(const std::size_t j) const
+  {
+    for (std::size_t i = 0; i <= j; ++i) {
+      typename CViewType::non_const_value_type sum = {};
+      for (std::size_t k = 0; k < Aview_.extent(1); ++k) {
+	sum += Aview_(i,k) * Aview_(j,k);
+      }
+      Cview_(i,j) = beta_*Cview_(i,j) + alpha_*sum;
+    }
+  }
+};
+
+template <class AlphaType, class BetaType, class AViewType, class CViewType>
+struct SyrkFunctor2
+{
+  AlphaType alpha_;
+  BetaType beta_;
+  AViewType Aview_;
+  CViewType Cview_;
+  SyrkFunctor2(AViewType Av, CViewType Cv,
+	       AlphaType alpha, BetaType beta)
+    : Aview_(Av), Cview_(Cv), alpha_(alpha), beta_(beta){}
+
+  KOKKOS_FUNCTION void operator()(const std::size_t j) const
+  {
+    for (std::size_t i = 0; i <= j; ++i) {
+      typename CViewType::non_const_value_type sum = {};
+      for (std::size_t k = 0; k < Aview_.extent(0); ++k) {
+	sum += Aview_(k,i) * Aview_(k,j);
+      }
+      Cview_(i,j) = beta_*Cview_(i,j) + alpha_*sum;
+    }
+  }
+};
+
+}// end namespace impl
+
+template<class AViewType, class CViewType>
+void syrk_kokkos(const char uplo[],
+		 const char opA[],
+		 typename AViewType::const_value_type & alpha,
+		 const AViewType& A,
+		 typename CViewType::const_value_type & beta,
+		 const CViewType& C)
+{
+  static_assert(    std::is_floating_point_v<typename AViewType::const_value_type>
+		 && std::is_floating_point_v<typename CViewType::const_value_type>,
+		 "syrk_kokkos: A and C currently must be floating point matrices");
+
+  static_assert(Kokkos::is_view_v<AViewType>,
+                "syrk_kokkos: AViewType must be a Kokkos::View.");
+  static_assert(Kokkos::is_view_v<CViewType>,
+                "syrk_kokkos: CViewType must be a Kokkos::View.");
+  static_assert(static_cast<int>(AViewType::rank) == 2,
+                "syrk_kokkos: AViewType must have rank 2.");
+  static_assert(static_cast<int>(CViewType::rank) == 2,
+                "syrk_kokkos: CViewType must have rank 2.");
+
+  if (C.extent(0) != C.extent(1)){
+    throw std::runtime_error("syrk_kokkos: C must be symmetric");
+  }
+
+  if (opA[0] != 'N' && opA[0] != 'T'){
+    throw std::runtime_error("syrk_kokkos: opA must be 'N' or 'T'");
+  }
+
+  if (uplo[0] != 'U'){
+    throw std::runtime_error("syrk_kokkos: currently uplo must be 'U'");
+  }
+
+  if (opA[0] == 'N' && C.extent(0) != A.extent(0)){
+    throw std::runtime_error("syrk_kokkos: opA=N : A.extent(0) should equal C.extent(0)");
+  }
+  if (opA[0] == 'T' && C.extent(0) != A.extent(1)){
+    throw std::runtime_error("syrk_kokkos: opA=T : A.extent(1) should equal C.extent(0)");
+  }
+
+  if (opA[0] == 'N'){
+    // symmetric rank-k update: C := alpha*A*A' + beta*C
+    using alpha_t = typename AViewType::const_value_type;
+    using beta_t  = typename CViewType::const_value_type;
+    using func_t = impl::SyrkFunctor1<alpha_t, beta_t, AViewType, CViewType>;
+    Kokkos::parallel_for(Kokkos::RangePolicy(0, C.extent(1)),
+			 func_t(A, C, alpha, beta));
+  }
+
+  else if (opA[0] == 'T'){
+    using alpha_t = typename AViewType::const_value_type;
+    using beta_t  = typename CViewType::const_value_type;
+    using func_t = impl::SyrkFunctor2<alpha_t, beta_t, AViewType, CViewType>;
+    Kokkos::parallel_for(Kokkos::RangePolicy(0, C.extent(1)),
+			 func_t(A, C, alpha, beta));
+  }
+}
+
+}// end namespace Tucker
+#endif
--- src/kokkos/src/impl/TuckerOnNode_compute_gram_kokkos_impl.hpp
+++ src/kokkos/src/impl/TuckerOnNode_compute_gram_kokkos_impl.hpp
@@ -3,56 +3,10 @@
 
 #include "Tucker_BlasWrapper.hpp"
 #include "TuckerOnNode_Tensor.hpp"
+#include "Tucker_syrk_kokkos.hpp"
 
 namespace TuckerOnNode{
 namespace impl{
-
-template <class ScalarType, class AViewType, class CViewType>
-struct FunctorGramModeZero
-{
-  ScalarType alpha_;
-  ScalarType beta_;
-  AViewType Aview_;
-  CViewType Cview_;
-  FunctorGramModeZero(AViewType Av, CViewType Cv,
-		      ScalarType alpha, ScalarType beta) :
-    Aview_(Av), Cview_(Cv), alpha_(alpha), beta_(beta){}
-
-  KOKKOS_FUNCTION void operator()(const std::size_t j) const
-  {
-    for (std::size_t i = 0; i <= j; ++i) {
-      ScalarType sum = {};
-      for (std::size_t k = 0; k < Aview_.extent(1); ++k) {
-	sum += Aview_(i,k) * Aview_(j,k);
-      }
-      Cview_(i,j) = beta_*Cview_(i,j) + alpha_*sum;
-    }
-
-  }
-};
-
-template <class ScalarType, class AViewType, class CViewType>
-struct FunctorGramModeNonZero
-{
-  ScalarType alpha_;
-  ScalarType beta_;
-  AViewType Aview_;
-  CViewType Cview_;
-  FunctorGramModeNonZero(AViewType Av, CViewType Cv,
-		      ScalarType alpha, ScalarType beta) :
-    Aview_(Av), Cview_(Cv), alpha_(alpha), beta_(beta){}
-
-  KOKKOS_FUNCTION void operator()(const std::size_t j) const
-  {
-    for (std::size_t i = 0; i <= j; ++i) {
-      ScalarType sum = {};
-      for (std::size_t k = 0; k < Aview_.extent(0); ++k) {
-	sum += Aview_(k,i) * Aview_(k,j);
-      }
-      Cview_(i,j) = beta_*Cview_(i,j) + alpha_*sum;
-    }
-  }
-};
 
 template <class ScalarType, class DataType, class ...ViewProps, class ...Properties>
 void compute_gram_kokkos(Tensor<ScalarType, Properties...> Y,
@@ -76,21 +30,16 @@ void compute_gram_kokkos(Tensor<ScalarType, Properties...> Y,
     // but that seems like a bad decision
     int ncols = 1;
     for(int i=0; i<(int)Y.rank(); i++) {
-      if((std::size_t)i != n) {
-        ncols *= (int)Y.extent(i);
-      }
+      if((std::size_t)i != n) { ncols *= (int)Y.extent(i); }
     }
 
-    // symmetric rank-k update
-    //   C := alpha*A*A' + beta*C
+    // symmetric rank-k update: C := alpha*A*A' + beta*C
     // which corresponds to:
     //   call syrk('U', 'N', nrows, ncols, alpha, Aptr, nrows, beta, Cptr, C.extent(0))
     const ScalarType alpha = 1;
     const ScalarType beta = 0;
     umv_type Aview(Y.data().data(), Y.extent(0), ncols);
-    using func_t = FunctorGramModeZero<ScalarType, umv_type, C_view_type>;
-    Kokkos::parallel_for(Kokkos::RangePolicy(0, C.extent(1)),
-			 func_t(Aview, C, alpha, beta));
+    Tucker::syrk_kokkos("U", "N", alpha, Aview, beta, C);
   }
 
   else
@@ -105,19 +54,14 @@ void compute_gram_kokkos(Tensor<ScalarType, Properties...> Y,
     }
 
     for(int i=0; i<nmats; i++) {
-      // symmetric rank-k update as follows:
-      //   C := alpha*A'*A + beta*C
+      // symmetric rank-k update as follows: C := alpha*A'*A + beta*C
       // which corresponds to:
-      //   dsyrk('U', 'T', nrows, ncols, alpha,
-      //         Aptr, ncols, beta, Cptr, C.extent(0))
-
+      //   dsyrk('U', 'T', nrows, ncols, alpha, Aptr, ncols, beta, Cptr, C.extent(0))
       const ScalarType alpha = 1;
       const ScalarType beta = (i==0) ? 0 : 1;
       auto Aptr = Y_rawPtr+i*nrows*ncols;
       umv_type Aview(Aptr, ncols, nrows);
-      using func_t = FunctorGramModeNonZero<ScalarType, umv_type, C_view_type>;
-      Kokkos::parallel_for(Kokkos::RangePolicy(0, C.extent(1)),
-			   func_t(Aview, C, alpha, beta));
+      Tucker::syrk_kokkos("U", "T", alpha, Aview, beta, C);
    }
   }
 }
