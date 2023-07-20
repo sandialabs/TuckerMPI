18300b92e2dcd2636967d1b30cd46b5d04cf4647
Francesco Rizzi <fnrizzi@sandia.gov>
2023-07-20 19:23:29 +0200

    related to #30 and #31

--- src/kokkos/src/TuckerMpi_sthosvd.hpp
+++ src/kokkos/src/TuckerMpi_sthosvd.hpp
@@ -20,7 +20,7 @@ template <class ScalarType, class ...Properties, class TruncatorType>
   using tensor_type = ::TuckerMpi::Tensor<ScalarType, Properties...>;
   using onnode_layout = typename tensor_type::traits::onnode_layout;
   static_assert(std::is_same_v<onnode_layout, Kokkos::LayoutLeft>,
-		"TuckerMpi::sthosvd: currently only supporting tensor with layoutleft");
+		"TuckerMpi::sthosvd: currently only supporting a tensor with LayoutLeft");
 
   // preconditions
   assert(modeOder.size() == (std::size_t) X.rank());
--- src/kokkos/src/TuckerOnNode_sthosvd.hpp
+++ src/kokkos/src/TuckerOnNode_sthosvd.hpp
@@ -15,6 +15,12 @@ template <class ScalarType, class ...Properties, class TruncatorType>
 			   TruncatorType && truncator,
 			   bool flipSign)
 {
+  // constraints
+  using tensor_type = ::TuckerOnNode::Tensor<ScalarType, Properties...>;
+  using layout = typename tensor_type::traits::array_layout;
+  static_assert(std::is_same_v<array_layout, Kokkos::LayoutLeft>,
+		"TuckerOnNode::sthosvd: currently only supporting a tensor with LayoutLeft");
+
   if (method == Method::Gram){
     return impl::sthosvd_gram(X, std::forward<TruncatorType>(truncator), flipSign);
   }
