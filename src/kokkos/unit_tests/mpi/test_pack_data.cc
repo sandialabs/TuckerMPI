a651cf5542b913b1b04455f26d51a7da0d2a9655
antoinemeyer5 <antoine.meyer54@gmail.com>
2023-07-24 13:34:41 +0200

    Translation with new code done

--- src/kokkos/unit_tests/mpi/test_pack_data.cc
+++ src/kokkos/unit_tests/mpi/test_pack_data.cc
@@ -8,37 +8,30 @@
 #include "TuckerMpi.hpp"
 #include "TuckerMpi_Map.hpp"
 
-const MPI_Comm comm = MPI_COMM_WORLD;
-
 TEST(tuckermpi_pack_data, main){
   // Prepare
   typedef double scalar_t;
   using memory_space = Kokkos::DefaultExecutionSpace::memory_space;
 
   // Create a 4x2x2 tensor
-  auto data_tensor_dim = {4, 2, 2};
-  auto proc_grid_dims = {2, 2, 2};
-  TuckerMpi::Tensor<scalar_t, memory_space> tensor(data_tensor_dim, proc_grid_dims);
+  std::vector<int> dims = {4, 2, 2};
+  TuckerOnNode::Tensor<scalar_t> tensor(dims);
 
   // Fill it with the entries 0:15
-  auto local_tensor_view_d = tensor.localTensor().data();
-  auto local_tensor_view_h = Kokkos::create_mirror(local_tensor_view_d);
+  auto tensor_d = tensor.data();
   for(int i=0; i<16; i++){
-    // ERROR HERE
-    local_tensor_view_h(i) = i;
-    // Who can I modify data of a TuckerMpi::Tensor?
+    tensor_d(i) = i;
   }
-  Kokkos::deep_copy(local_tensor_view_d, local_tensor_view_h);
 
   // Create a map describing the distribution
-  TuckerMpi::Map m(4, comm);
+  TuckerMpi::Map map(4, MPI_COMM_WORLD);
 
   // Pack the tensor
-  TuckerMpi::impl::packForTTM(tensor.localTensor(), 0, &m);
+  TuckerMpi::impl::packForTTM(tensor, 0, &map);
 
   // Check the result
   scalar_t trueResult[16] = {0,1,4,5,8,9,12,13,2,3,6,7,10,11,14,15};
   for(int i=0; i<16; i++) {
-    ASSERT_TRUE(local_tensor_view_d[i] == trueResult[i]);
+    ASSERT_TRUE(tensor_d[i] == trueResult[i]);
   }
 }
