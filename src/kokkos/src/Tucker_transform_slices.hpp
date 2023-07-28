35968ae981c5c04eb3f40135bf43477bff226e72
antoinemeyer5 <antoine.meyer54@gmail.com>
2023-07-28 12:29:42 +0200

    Kokkos work in progress #52

--- src/kokkos/src/Tucker_transform_slices.hpp
+++ src/kokkos/src/Tucker_transform_slices.hpp
@@ -7,7 +7,9 @@ namespace Tucker {
 
 // Shift is applied before scale
 // We divide by scaleVals, not multiply
-template<class ScalarType, class ... TensorParams, class ViewDataType, class ... ViewParams>
+template<
+  class ScalarType, class ... TensorParams,
+  class ViewDataType, class ... ViewParams>
 void transform_slices(TuckerOnNode::Tensor<ScalarType, TensorParams...> tensor,
                       int mode,
                       const Kokkos::View<ViewDataType, ViewParams...> scales,
@@ -23,40 +25,44 @@ void transform_slices(TuckerOnNode::Tensor<ScalarType, TensorParams...> tensor,
   int numSlices = tensor.extent(mode);
   // Number of contiguous elements in a slice
   size_t numContig = tensor.prod(0,mode-1,1);
-  std::cout << "numContig: " << numContig << std::endl;
   // Number of sets of contiguous elements per slice
   size_t numSetsContig = tensor.prod(mode+1,ndims-1,1);
-  std::cout << "numSetsContig: " << numSetsContig << std::endl;
   // Distance between sets of contiguous elements
   size_t distBetweenSets = tensor.prod(0,mode);
-  std::cout << "distBetweenSets: " << distBetweenSets << std::endl;
 
   ScalarType* dataPtr;
   int slice;
   size_t i, c;
-  #pragma omp parallel for default(shared) private(slice,i,c,dataPtr)
   for(slice=0; slice<numSlices; slice++)
   {
     dataPtr = tensor.data().data() + slice*numContig;
-    for(c=0; c<numSetsContig; c++)
+    for(size_t c=0; c<numSetsContig; c++)
     {
-      for(i=0; i<numContig; i++)
+      for(size_t i=0; i<numContig; i++){
         dataPtr[i] = (dataPtr[i] + shifts[slice]) / scales[slice];
+      }
       dataPtr += distBetweenSets;
     }
   }
-  //
 
-  // parallel_for(
-  //   just on tensor data
-  // )
+  // WIP
+  // Kokkos idea
+  /*
+  Kokkos::View<ViewDataType, ViewParams...> dataPtr("data view", numEntries);
+  Kokkos::RangePolicy<> policy_1(0, numSlices);
+  Kokkos::parallel_for(
+    "Loop",
+    policy_1,
+    KOKKOS_LAMBDA(const int slice) {
+      std::cout << "slice: " << slice << std::endl;
 
+    }
+  );
+  */
+  // typedef Kokkos::TeamPolicy<>::member_type team_handle;
+  // Kokkos::TeamPolicy<> policy_1(numSlices, Kokkos::impl::AUTO, numSetsContig);
 
-  // DONE 1) FINIR CODE
-  // DONE 2) TEST PASSE NORMALEMENT
-  // 3) METTRE FOOR LOOP IN CMAKELISTS
-  // 4) AJOUTER ANCIEN TESTS
-
+  // WIP
 }
 
 }
