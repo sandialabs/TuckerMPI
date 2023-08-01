2903b3d84b50e695c189b5d23cd88aa4bc662b13
Francesco Rizzi <fnrizzi@sandia.gov>
2023-08-01 14:55:05 +0200

    finalize for now

--- src/kokkos/src/Tucker_transform_slices.hpp
+++ src/kokkos/src/Tucker_transform_slices.hpp
@@ -1,155 +1,47 @@
 #ifndef TUCKER_KOKKOS_TRANSFORM_SLICES_HPP_
 #define TUCKER_KOKKOS_TRANSFORM_SLICES_HPP_
 
-#include "TuckerOnNode_Tensor.hpp"
+#include "./impl/Tucker_transform_slices.hpp"
 
-#include <Kokkos_Core.hpp>
-#include <Kokkos_StdAlgorithms.hpp>
+namespace TuckerOnNode{
 
-#include <cuchar>
-
-namespace Tucker {
-
-namespace impl{
-
-// FIXME: this needs to be improved with a team level and nested reductions
 template<
-  class ItType, class DividingScaleView, class ShiftValuesBeforeScaleView>
-struct Func{
-  ItType itBegin_;
-  std::size_t numContig_;
-  std::size_t numSetsContig_;
-  std::size_t distBetweenSets_;
-  DividingScaleView dividing_scales_;
-  ShiftValuesBeforeScaleView shift_values_before_scale_;
+  class ScalarType, class ... TensorProps,
+  class ViewDataType1, class ... ViewParams1,
+  class ViewDataType2, class ... ViewParams2
+  >
+void transform_slices(Tensor<ScalarType, TensorProps...> tensor,
+                      int mode,
+                      const Kokkos::View<ViewDataType1, ViewParams1...> & dividing_scales,
+                      const Kokkos::View<ViewDataType2, ViewParams2...> & pre_scaling_shifts)
+{
+  using tensor_type = Tensor<ScalarType, TensorProps...>;
+  using tensor_layout = typename tensor_type::traits::array_layout;
+  using tensor_value_type = typename tensor_type::traits::value_type;
 
-  Func(ItType it, std::size_t numContig, std::size_t numSetsContig,
-       std::size_t distBetweenSets, DividingScaleView dividing_scales,
-       ShiftValuesBeforeScaleView shift_values_before_scale)
-    : itBegin_(it), numContig_(numContig), numSetsContig_(numSetsContig),
-      distBetweenSets_(distBetweenSets), dividing_scales_(dividing_scales), shift_values_before_scale_(shift_values_before_scale){}
+  // constraints
+  static_assert(   std::is_same_v<tensor_layout, Kokkos::LayoutLeft>
+		&& std::is_floating_point_v<tensor_value_type>,
+		   "TuckerOnNode::transform_slices: supports tensors with LayoutLeft" \
+		   "and floating point scalar");
 
-  KOKKOS_FUNCTION void operator()(int sliceIndex) const
-  {
-    auto it = itBegin_ + sliceIndex*numContig_;
-    for(std::size_t c=0; c<numSetsContig_; c++)
-      {
-        for(std::size_t i=0; i<numContig_; i++)
-        {
-          auto & value = *(it+i);
-          value = (value + shift_values_before_scale_(sliceIndex)) / dividing_scales_(sliceIndex);
-        } // end for i
-        it += distBetweenSets_;
-      } // end for c
+  // preconditions
+  if(tensor.extent(mode) <= 0) {
+    std::ostringstream oss;
+    oss << "TuckerOnNode::transform_slices: "
+        << "for mode = " << mode << " we have tensor.extent(mode) = " << tensor.extent(mode) << " <= 0";
+    throw std::runtime_error(oss.str());
   }
-};
 
+  if(mode < 0) {
+    throw std::runtime_error("mode must be non-negative");
+  }
 
-template<
-  class ScalarType, class ... TensorParams,
-  class ViewDataType, class ... ViewParams>
-void transform_slices(TuckerOnNode::Tensor<ScalarType, TensorParams...> Y,
-                      int mode,
-                      const Kokkos::View<ViewDataType, ViewParams...> dividing_scales,
-                      const Kokkos::View<ViewDataType, ViewParams...> shift_values_before_scale)
-{
-
-  const int numSlices = Y.extent(mode);
-  const int ndims = Y.rank();
-  // Number of contiguous elements in a slice
-  std::size_t numContig = Y.prod(0,mode-1,1);
-  // Number of sets of contiguous elements per slice
-  std::size_t numSetsContig = Y.prod(mode+1,ndims-1,1);
-  // Distance between sets of contiguous elements
-  std::size_t distBetweenSets = Y.prod(0,mode);
-
-  auto itBegin = Kokkos::Experimental::begin(Y.data());
-  Kokkos::parallel_for(numSlices,
-		       Func(itBegin, numContig, numSetsContig, distBetweenSets,
-			    dividing_scales, shift_values_before_scale));
-
-};
-
-} // end of impl
-
-
-template<
-  class ScalarType, class ... TensorParams,
-  class ViewDataType, class ... ViewParams>
-void transform_slices(TuckerOnNode::Tensor<ScalarType, TensorParams...> tensor,
-                      int mode,
-                      const Kokkos::View<ViewDataType, ViewParams...> dividing_scales,
-                      const Kokkos::View<ViewDataType, ViewParams...> shift_values_before_scale)
-{
-
-  impl::transform_slices(tensor, mode, dividing_scales, shift_values_before_scale);
+  // execute
+  if(tensor.size() > 0) {
+    impl::transform_slices(tensor, mode, dividing_scales, pre_scaling_shifts);
+  }
 }
 
 } // end of tucker
 #endif
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // If the tensor has no entries, no transformation is necessary
-  size_t numEntries = tensor.size();
-  if(numEntries == 0)
-    return;
-
-  // Compute the result
-  int ndims = tensor.rank();
-  int numSlices = tensor.extent(mode);
-  // Number of contiguous elements in a slice
-  size_t numContig = tensor.prod(0,mode-1,1);
-  // Number of sets of contiguous elements per slice
-  size_t numSetsContig = tensor.prod(mode+1,ndims-1,1);
-  // Distance between sets of contiguous elements
-  size_t distBetweenSets = tensor.prod(0,mode);
-
-  ScalarType* dataPtr;
-  int slice;
-  size_t i, c;
-  for(slice=0; slice<numSlices; slice++) // parallel_for
-  {
-    dataPtr = tensor.data().data() + slice*numContig;
-    for(size_t c=0; c<numSetsContig; c++)
-    {
-      for(size_t i=0; i<numContig; i++){
-        dataPtr[i] = (dataPtr[i] + shifts[slice]) / scales[slice];
-      }
-      dataPtr += distBetweenSets;
-    }
-  }
-
-  // WIP
-  // Kokkos idea
-  /*
-  Kokkos::View<ViewDataType, ViewParams...> dataPtr("data view", numEntries);
-  Kokkos::RangePolicy<> policy_1(0, numSlices);
-  Kokkos::parallel_for(
-    "Loop",
-    policy_1,
-    KOKKOS_LAMBDA(const int slice) {
-      std::cout << "slice: " << slice << std::endl;
-
-    }
-  );
-
-  // typedef Kokkos::TeamPolicy<>::member_type team_handle;
-  // Kokkos::TeamPolicy<> policy_1(numSlices, Kokkos::impl::AUTO, numSetsContig);
-
-  // WIP
-}
-
-}*/
--- src/kokkos/unit_tests/onnode/test_transform_slices_3x5x7x11/test_transform_slices.cc
+++ src/kokkos/unit_tests/onnode/test_transform_slices_3x5x7x11/test_transform_slices.cc
@@ -3,31 +3,32 @@
  * https://gitlab.com/tensors/TuckerMPI/-/blob/master/src/serial/tests/Tucker_shift_scale_test.cpp
  */
 
+#include <gtest/gtest.h>
 #include "TuckerOnNode.hpp"
 
-#include <gtest/gtest.h>
-
-
-template<class ScalarType, class MemorySpaces>
-bool checks(TuckerOnNode::Tensor<ScalarType, MemorySpaces> t1,
-            TuckerOnNode::Tensor<ScalarType, MemorySpaces> t2,
+template<class ScalarType, class ...Props1, class ...Props2>
+bool checks(TuckerOnNode::Tensor<ScalarType, Props1...> t1,
+            TuckerOnNode::Tensor<ScalarType, Props2...> t2,
             ScalarType tol)
 {
+  auto t1_h = Tucker::create_mirror_and_copy(Kokkos::HostSpace(), t1);
+  auto t2_h = Tucker::create_mirror_and_copy(Kokkos::HostSpace(), t2);
+
   // 1) Owns any data
-  if(t1.size() == 0 && t2.size() == 0){
+  if(t1_h.size() == 0 && t2_h.size() == 0){
     return true;
   }
 
   // 2) Same size
-  if(t1.size() != t2.size()) {
+  if(t1_h.size() != t2_h.size()) {
     return false;
   }
 
   // 3) Values
-  int numElements = t1.size();
+  int numElements = t1_h.size();
   ScalarType errNorm2 = 0;
   for(int i=0; i<numElements; i++) {
-    ScalarType err = std::abs(t1.data()[i] - t2.data()[i]);
+    ScalarType err = std::abs(t1_h.data()[i] - t2_h.data()[i]);
     if(std::isnan(err)){
       return false;
     }
@@ -35,7 +36,7 @@ bool checks(TuckerOnNode::Tensor<ScalarType, MemorySpaces> t1,
   }
 
   // 4) Tol
-  ScalarType origNorm2 = t1.frobeniusNormSquared();
+  ScalarType origNorm2 = t1_h.frobeniusNormSquared();
   ScalarType relErr = std::sqrt(errNorm2/origNorm2);
   if(relErr > tol){
     return false;
@@ -45,158 +46,143 @@ bool checks(TuckerOnNode::Tensor<ScalarType, MemorySpaces> t1,
   return true;
 }
 
-TEST(tuckerkokkos, transform_slices_3x5x7x11_mode0){
-  // Prepare
-  typedef double scalar_t;
-  using memory_space = Kokkos::DefaultExecutionSpace::memory_space;
+TEST(tuckerkokkos, transform_slices_3x5x7x11_mode0)
+{
+  using scalar_t = double;
 
-  // Create a 3x5x7x11 tensor
   std::vector<int> dims = {3, 5, 7, 11};
-  TuckerOnNode::Tensor<scalar_t, memory_space> tensor(dims);
-
-  // Read tensor from file
+  TuckerOnNode::Tensor<scalar_t> tensor(dims);
   TuckerOnNode::import_tensor_binary(tensor, "./gold_3x5x7x11.bin");
 
   // Define scales and shifts
-  Kokkos::View<scalar_t*, memory_space> scales("scales", 3);
-  scales(0) = -0.258915944830978;
-  scales(1) =  0.341369101972144;
-  scales(2) =  0.357212764090606;
+  Kokkos::View<scalar_t*> scales("scales", 3);
+  auto scales_h = Kokkos::create_mirror(scales);
+  scales_h(0) = -0.258915944830978;
+  scales_h(1) =  0.341369101972144;
+  scales_h(2) =  0.357212764090606;
+  Kokkos::deep_copy(scales, scales_h);
 
-  Kokkos::View<scalar_t*, memory_space> shifts("shifts", 3);
-  shifts(0) = -0.450368098480610;
-  shifts(1) = -0.408899824586946;
-  shifts(2) =  0.094037031444121;
+  Kokkos::View<scalar_t*> shifts("shifts", 3);
+  auto shifts_h = Kokkos::create_mirror(shifts);
+  shifts_h(0) = -0.450368098480610;
+  shifts_h(1) = -0.408899824586946;
+  shifts_h(2) =  0.094037031444121;
+  Kokkos::deep_copy(shifts, shifts_h);
 
   // Read true solution from file
-  TuckerOnNode::Tensor<scalar_t, memory_space> true_sol(dims);
-  TuckerOnNode::import_tensor_binary(true_sol, "./gold_3x5x7x11_ss0.bin");
+  TuckerOnNode::Tensor<scalar_t, Kokkos::HostSpace> true_sol_h(dims);
+  TuckerOnNode::import_tensor_binary(true_sol_h, "./gold_3x5x7x11_ss0.bin");
 
-  // Call shift-scale
-  int mode = 0;
-  Tucker::transform_slices(tensor, mode, scales, shifts);
+  const int mode = 0;
+  TuckerOnNode::transform_slices(tensor, mode, scales, shifts);
 
-  // Checks
   scalar_t tol = 100 * std::numeric_limits<scalar_t>::epsilon();
-  ASSERT_TRUE(checks(tensor, true_sol, tol));
+  ASSERT_TRUE(checks(tensor, true_sol_h, tol));
 }
 
-TEST(tuckerkokkos, transform_slices_3x5x7x11_mode1){
-  // Prepare
-  typedef double scalar_t;
-  using memory_space = Kokkos::DefaultExecutionSpace::memory_space;
+TEST(tuckerkokkos, transform_slices_3x5x7x11_mode1)
+{
+  using scalar_t = double;
 
-  // Create a 3x5x7x11 tensor
   std::vector<int> dims = {3, 5, 7, 11};
-  TuckerOnNode::Tensor<scalar_t, memory_space> tensor(dims);
-
-  // Read tensor from file
+  TuckerOnNode::Tensor<scalar_t> tensor(dims);
   TuckerOnNode::import_tensor_binary(tensor, "./gold_3x5x7x11.bin");
 
-  // Define scales and shifts
-  Kokkos::View<scalar_t*, memory_space> scales("scales", 5);
-  scales(0) =  0.262109709211147;
-  scales(1) = -0.152432849551241;
-  scales(2) = -0.038768240608501;
-  scales(3) =  0.139323762199356;
-  scales(4) =  0.417336040866845;
+  Kokkos::View<scalar_t*> scales("scales", 5);
+  auto scales_h = Kokkos::create_mirror(scales);
+  scales_h(0) =  0.262109709211147;
+  scales_h(1) = -0.152432849551241;
+  scales_h(2) = -0.038768240608501;
+  scales_h(3) =  0.139323762199356;
+  scales_h(4) =  0.417336040866845;
+  Kokkos::deep_copy(scales, scales_h);
 
-  Kokkos::View<scalar_t*, memory_space> shifts("shifts", 5);
-  shifts(0) =  0.463612200951355;
-  shifts(1) = -0.011100213839396;
-  shifts(2) = -0.279689899431367;
-  shifts(3) = -0.273791359158714;
-  shifts(4) =  0.036787804512826;
+  Kokkos::View<scalar_t*> shifts("shifts", 5);
+  auto shifts_h = Kokkos::create_mirror(shifts);
+  shifts_h(0) =  0.463612200951355;
+  shifts_h(1) = -0.011100213839396;
+  shifts_h(2) = -0.279689899431367;
+  shifts_h(3) = -0.273791359158714;
+  shifts_h(4) =  0.036787804512826;
+  Kokkos::deep_copy(shifts, shifts_h);
 
-  // Read true solution from file
-  TuckerOnNode::Tensor<scalar_t, memory_space> true_sol(dims);
-  TuckerOnNode::import_tensor_binary(true_sol, "./gold_3x5x7x11_ss1.bin");
+  TuckerOnNode::Tensor<scalar_t> true_sol_h(dims);
+  TuckerOnNode::import_tensor_binary(true_sol_h, "./gold_3x5x7x11_ss1.bin");
 
-  // Call shift-scale
-  int mode = 1;
-  Tucker::transform_slices(tensor, mode, scales, shifts);
+  const int mode = 1;
+  TuckerOnNode::transform_slices(tensor, mode, scales, shifts);
 
-  // Checks
   scalar_t tol = 100 * std::numeric_limits<scalar_t>::epsilon();
-  ASSERT_TRUE(checks(tensor, true_sol, tol));
+  ASSERT_TRUE(checks(tensor, true_sol_h, tol));
 }
 
-TEST(tuckerkokkos, transform_slices_3x5x7x11_mode2){
-  // Prepare
-  typedef double scalar_t;
-  using memory_space = Kokkos::DefaultExecutionSpace::memory_space;
-
-  // Create a 3x5x7x11 tensor
+TEST(tuckerkokkos, transform_slices_3x5x7x11_mode2)
+{
+  using scalar_t = double;
   std::vector<int> dims = {3, 5, 7, 11};
-  TuckerOnNode::Tensor<scalar_t, memory_space> tensor(dims);
-
-  // Read tensor from file
+  TuckerOnNode::Tensor<scalar_t> tensor(dims);
   TuckerOnNode::import_tensor_binary(tensor, "./gold_3x5x7x11.bin");
 
-  // Define scales and shifts
-  Kokkos::View<scalar_t*, memory_space> scales("scales", 7);
-  scales(0) =  0.133333580320122; scales(1) =  0.124000554312344;
-  scales(2) = -0.172058403026751; scales(3) =  0.302965315958237;
-  scales(4) =  0.499477858635892; scales(5) =  0.480978160932146;
-  scales(6) = -0.372963057805143;
+  Kokkos::View<scalar_t*> scales("scales", 7);
+  auto scales_h = Kokkos::create_mirror(scales);
+  scales_h(0) =  0.133333580320122; scales_h(1) =  0.124000554312344;
+  scales_h(2) = -0.172058403026751; scales_h(3) =  0.302965315958237;
+  scales_h(4) =  0.499477858635892; scales_h(5) =  0.480978160932146;
+  scales_h(6) = -0.372963057805143;
+  Kokkos::deep_copy(scales, scales_h);
 
-  Kokkos::View<scalar_t*, memory_space> shifts("shifts", 7);
-  shifts(0) = -0.338427426109669; shifts(1) =  0.215635404167474;
-  shifts(2) =  0.077738876192409; shifts(3) = -0.066701022790881;
-  shifts(4) =  0.384242782631094; shifts(5) = -0.106948244623087;
-  shifts(6) = -0.321024847372268;
+  Kokkos::View<scalar_t*> shifts("shifts", 7);
+  auto shifts_h = Kokkos::create_mirror(shifts);
+  shifts_h(0) = -0.338427426109669; shifts_h(1) =  0.215635404167474;
+  shifts_h(2) =  0.077738876192409; shifts_h(3) = -0.066701022790881;
+  shifts_h(4) =  0.384242782631094; shifts_h(5) = -0.106948244623087;
+  shifts_h(6) = -0.321024847372268;
+  Kokkos::deep_copy(shifts, shifts_h);
 
-  // Read true solution from file
-  TuckerOnNode::Tensor<scalar_t, memory_space> true_sol(dims);
-  TuckerOnNode::import_tensor_binary(true_sol, "./gold_3x5x7x11_ss2.bin");
+  TuckerOnNode::Tensor<scalar_t> true_sol_h(dims);
+  TuckerOnNode::import_tensor_binary(true_sol_h, "./gold_3x5x7x11_ss2.bin");
 
-  // Call shift-scale
-  int mode = 2;
-  Tucker::transform_slices(tensor, mode, scales, shifts);
+  const int mode = 2;
+  TuckerOnNode::transform_slices(tensor, mode, scales, shifts);
 
-  // Checks
   scalar_t tol = 100 * std::numeric_limits<scalar_t>::epsilon();
-  ASSERT_TRUE(checks(tensor, true_sol, tol));
+  ASSERT_TRUE(checks(tensor, true_sol_h, tol));
 }
 
-TEST(tuckerkokkos, transform_slices_3x5x7x11_mode3){
-  // Prepare
-  typedef double scalar_t;
-  using memory_space = Kokkos::DefaultExecutionSpace::memory_space;
+TEST(tuckerkokkos, transform_slices_3x5x7x11_mode3)
+{
+  using scalar_t = double;
 
-  // Create a 3x5x7x11 tensor
   std::vector<int> dims = {3, 5, 7, 11};
-  TuckerOnNode::Tensor<scalar_t, memory_space> tensor(dims);
-
-  // Read tensor from file
+  TuckerOnNode::Tensor<scalar_t> tensor(dims);
   TuckerOnNode::import_tensor_binary(tensor, "./gold_3x5x7x11.bin");
 
-  // Define scales and shifts
-  Kokkos::View<scalar_t*, memory_space> scales("scales", 11);
-  scales(0) = -0.375974083351957; scales(1) = -0.029236754133043;
-  scales(2) =  0.356896327782193; scales(3) = -0.456609528433047;
-  scales(4) =  0.191625145201306; scales(5) =  0.478985466675038;
-  scales(6) = -0.216732101507863; scales(7) = -0.366219500005577;
-  scales(8) =  0.185279684412687; scales(9) =  0.409454555749395;
-  scales(10) = 0.110868982383243;
+  Kokkos::View<scalar_t*> scales("scales", 11);
+  auto scales_h = Kokkos::create_mirror(scales);
+  scales_h(0) = -0.375974083351957; scales_h(1) = -0.029236754133043;
+  scales_h(2) =  0.356896327782193; scales_h(3) = -0.456609528433047;
+  scales_h(4) =  0.191625145201306; scales_h(5) =  0.478985466675038;
+  scales_h(6) = -0.216732101507863; scales_h(7) = -0.366219500005577;
+  scales_h(8) =  0.185279684412687; scales_h(9) =  0.409454555749395;
+  scales_h(10) = 0.110868982383243;
+  Kokkos::deep_copy(scales, scales_h);
 
-  Kokkos::View<scalar_t*, memory_space> shifts("shifts", 11);
-  shifts(0) = -0.267759854038207; shifts(1) = -0.476367533341775;
-  shifts(2) =  0.107432610401855; shifts(3) = -0.389190678712850;
-  shifts(4) = -0.092540492121601; shifts(5) =  0.384076806661962;
-  shifts(6) =  0.048132777476588; shifts(7) = -0.130996923288383;
-  shifts(8) = -0.291654017186653; shifts(9) = -0.059056723475676;
-  shifts(10) =  0.456196152175878;
+  Kokkos::View<scalar_t*> shifts("shifts", 11);
+  auto shifts_h = Kokkos::create_mirror(shifts);
+  shifts_h(0) = -0.267759854038207; shifts_h(1) = -0.476367533341775;
+  shifts_h(2) =  0.107432610401855; shifts_h(3) = -0.389190678712850;
+  shifts_h(4) = -0.092540492121601; shifts_h(5) =  0.384076806661962;
+  shifts_h(6) =  0.048132777476588; shifts_h(7) = -0.130996923288383;
+  shifts_h(8) = -0.291654017186653; shifts_h(9) = -0.059056723475676;
+  shifts_h(10) =  0.456196152175878;
+  Kokkos::deep_copy(shifts, shifts_h);
 
-  // Read true solution from file
-  TuckerOnNode::Tensor<scalar_t, memory_space> true_sol(dims);
-  TuckerOnNode::import_tensor_binary(true_sol, "./gold_3x5x7x11_ss3.bin");
+  TuckerOnNode::Tensor<scalar_t> true_sol_h(dims);
+  TuckerOnNode::import_tensor_binary(true_sol_h, "./gold_3x5x7x11_ss3.bin");
 
-  // Call shift-scale
-  int mode = 3;
-  Tucker::transform_slices(tensor, mode, scales, shifts);
+  const int mode = 3;
+  TuckerOnNode::transform_slices(tensor, mode, scales, shifts);
 
-  // Checks
   scalar_t tol = 100 * std::numeric_limits<scalar_t>::epsilon();
-  ASSERT_TRUE(checks(tensor, true_sol, tol));
+  ASSERT_TRUE(checks(tensor, true_sol_h, tol));
 }
