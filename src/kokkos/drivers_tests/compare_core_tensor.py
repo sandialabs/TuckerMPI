bd72efb705dce6a9d0430b49b3dd8eff22f3c619
antoinemeyer5 <antoine.meyer54@gmail.com>
2023-08-02 14:29:02 +0200

    wip #54

--- src/kokkos/drivers_tests/compare_core_tensor.py
+++ src/kokkos/drivers_tests/compare_core_tensor.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+from argparse import ArgumentParser
 
 def extract_sthosvd_path(paramfile):
     with open(paramfile) as fh:
@@ -17,9 +18,16 @@ def extract_sthosvd_prefix(paramfile):
 
 if __name__== "__main__":
 
+    parser = ArgumentParser()
+    parser.add_argument("--rtol", dest="relTol", type=float)
+    parser.add_argument("--atol", dest="absTol", type=float)
+    args = parser.parse_args()
+
     # figure out where sthosvd values are written to
     sthosvdPath     = extract_sthosvd_path("./paramfile.txt")
     sthosvdPrefix   = extract_sthosvd_prefix("./paramfile.txt")
+    print("STHOSVD directory = {}".format(sthosvdPath))
+    print("STHOSVD file prefix = {}".format(sthosvdPrefix))
 
     # convert real data into text file
     binary = np.fromfile(sthosvdPath + "/" + sthosvdPrefix + "_core.mpi")
@@ -32,5 +40,5 @@ if __name__== "__main__":
     # compare
     assert(len(gold) == len(computed)), \
         "Failing due to mismatching extents!"
-    assert(np.allclose(gold, computed, rtol=1e-9, atol=1e-11)), \
+    assert(np.allclose(gold, computed, rtol=args.relTol, atol=args.absTol)), \
         "Failing due to numerical differences!"
