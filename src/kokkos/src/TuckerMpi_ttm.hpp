6a01966f9f225edf9d56eb729ea6c3b4cc59c59b
Francesco Rizzi <fnrizzi@sandia.gov>
2023-08-23 05:02:50 -0700

    fix ttm

--- src/kokkos/src/TuckerMpi_ttm.hpp
+++ src/kokkos/src/TuckerMpi_ttm.hpp
@@ -8,14 +8,15 @@ namespace TuckerMpi{
 
 template <class ScalarType, class ...TensorProperties, class ...ViewProperties>
 auto ttm(Tensor<ScalarType, TensorProperties...> Xtensor,
-	 int n,
-	 Kokkos::View<ScalarType**, ViewProperties...> Umatrix,
-	 bool Utransp,
-	 std::size_t nnz_limit)
+         int n,
+         Kokkos::View<ScalarType**, ViewProperties...> Umatrix,
+         bool Utransp,
+         std::size_t nnz_limit)
 {
 
   // constraints
   using tensor_type   = Tensor<ScalarType, TensorProperties...>;
+  using local_tensor_type = typename tensor_type::traits::onnode_tensor_type;
   using tensor_layout = typename tensor_type::traits::onnode_layout;
   using tensor_memory_space = typename tensor_type::traits::memory_space;
   static_assert(std::is_same_v<tensor_layout, Kokkos::LayoutLeft>,
@@ -25,15 +26,42 @@ auto ttm(Tensor<ScalarType, TensorProperties...> Xtensor,
   static_assert(std::is_same_v<tensor_memory_space, typename u_view_t::memory_space>,
 		"TuckerMpi::ttm: tensor and matrix arguments must have matching memory spaces");
 
+  int mpiRank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
+
   // Compute the number of rows for the resulting "matrix"
   const int nrows = Utransp ? Umatrix.extent(1) : Umatrix.extent(0);
+
+  // Get the size of the new tensor
   const int ndims = Xtensor.rank();
   std::vector<int> newSize(ndims);
   for(int i=0; i<ndims; i++) {
     newSize[i] = (i == n) ? nrows : Xtensor.globalExtent(i);
   }
-  Distribution dist(newSize, Xtensor.getDistribution().getProcessorGrid().getSizeArray());
-  tensor_type result(dist);
+
+  Distribution resultDist(newSize, Xtensor.getDistribution().getProcessorGrid().getSizeArray());
+  tensor_type result(resultDist);
+
+  auto preferSingleReduceScatter = [=]() -> bool
+  {
+    auto & Xdist = Xtensor.getDistribution();
+    std::size_t max_lcl_nnz_x = 1;
+    for(int i=0; i<Xtensor.rank(); i++) {
+      max_lcl_nnz_x *= Xdist.getMap(i,false)->getMaxNumEntries();
+    }
+    // Compute the nnz required for the reduce_scatter
+    std::size_t nnz_reduce_scatter = 1;
+    for(int i=0; i<Xtensor.rank(); i++) {
+      if(i == n){
+        nnz_reduce_scatter *= result.globalExtent(n);
+      }
+      else{
+        nnz_reduce_scatter *= Xdist.getMap(i,false)->getMaxNumEntries();
+      }
+    }
+
+    return (nnz_reduce_scatter <= std::max(max_lcl_nnz_x, nnz_limit));
+  };
 
   const int Pn = Xtensor.getDistribution().getProcessorGrid().getNumProcs(n, false);
   if(Pn == 1)
@@ -48,39 +76,16 @@ auto ttm(Tensor<ScalarType, TensorProperties...> Xtensor,
       TuckerOnNode::ttm(localX, n, Umatrix, localresult, Utransp);
     }
   }
-  else{
-
-    // FRIZZI: NOTE: reduce scatter has bad performance , needs to improve
+  else
+  {
+    // FRIZZI: NOTE: reduce scatter has bad performance, needs to figure out why
     // so always calling the series of reduction
-#if 0
-    std::size_t max_lcl_nnz_x = 1;
-    for(int i=0; i<Xtensor.rank(); i++) {
-      max_lcl_nnz_x *= Xtensor.getDistribution().getMap(i,false)->getMaxNumEntries();
-    }
-    // Compute the nnz required for the reduce_scatter
-    std::size_t nnz_reduce_scatter = 1;
-    for(int i=0; i<Xtensor.rank(); i++) {
-      if(i == n){
-        nnz_reduce_scatter *= result.globalExtent(n);
-      }
-      else{
-        nnz_reduce_scatter *= Xtensor.getDistribution().getMap(i,false)->getMaxNumEntries();
-      }
-    }
-
-    if(nnz_reduce_scatter <= std::max(max_lcl_nnz_x, nnz_limit)){
-      impl::ttm_impl_use_single_reduce_scatter(Xtensor, result, n, Umatrix, Utransp);
-    }
-    else{
-#endif
-
-    impl::ttm_impl_use_series_of_reductions(Xtensor, result, n, Umatrix, Utransp);
-
-#if 0
-    }
-#endif
+    // if (preferSingleReduceScatter()){
+    //   impl::ttm_impl_use_single_reduce_scatter(mpiRank, Xtensor, result, n, Umatrix, Utransp);
+    //} else{
+    impl::ttm_impl_use_series_of_reductions(mpiRank, Xtensor, result, n, Umatrix, Utransp);
+    //}
   }
-
   return result;
 }
 
