f79de310e321424523bd02017433d8c86e18169d
Francesco Rizzi <fnrizzi@sandia.gov>
2023-08-24 12:59:37 -0700

    improve readability

--- src/kokkos/src/TuckerOnNode_normalize_tensor.hpp
+++ src/kokkos/src/TuckerOnNode_normalize_tensor.hpp
@@ -17,32 +17,32 @@ template <class ScalarType, class MetricMemSpace, class ...Props>
 {
   // preconditions
   impl::check_scaling_type_else_throw(scalingType);
-
-  ::TuckerOnNode::impl::check_metricdata_usable_for_scaling_else_throw(metricsData, scalingType);
+  impl::check_metricdata_usable_for_scaling_else_throw(metricsData, scalingType);
 
   using tensor_type = TuckerOnNode::Tensor<ScalarType, Props...>;
   using tensor_mem_space = typename tensor_type::traits::memory_space;
 
-  //
   // 1. use metrics to fill scales and shifts depending on the scalingType
   Kokkos::View<ScalarType*, tensor_mem_space> scales("scales", tensor.extent(scaleMode));
   Kokkos::View<ScalarType*, tensor_mem_space> shifts("shifts", tensor.extent(scaleMode));
   if(scalingType == "Max") {
     impl::NormalizeFunc func(metricsData, scales, shifts);
-    Kokkos::parallel_for(Kokkos::RangePolicy<impl::UseMax>(0, tensor.extent(scaleMode)), func);
+    Kokkos::RangePolicy<impl::UseMax> policy(0, tensor.extent(scaleMode));
+    Kokkos::parallel_for(policy, func);
   }
 
   else if(scalingType == "MinMax") {
     impl::NormalizeFunc func(metricsData, scales, shifts);
-    Kokkos::parallel_for(Kokkos::RangePolicy<impl::UseMinMax>(0, tensor.extent(scaleMode)), func);
+    Kokkos::RangePolicy<impl::UseMinMax> policy(0, tensor.extent(scaleMode));
+    Kokkos::parallel_for(policy, func);
   }
 
   else if(scalingType == "StandardCentering") {
     impl::NormalizeFunc func(metricsData, scales, shifts, stdThresh);
-    Kokkos::parallel_for(Kokkos::RangePolicy<impl::UseStandardCentering>(0, tensor.extent(scaleMode)), func);
+    Kokkos::RangePolicy<impl::UseStandardCentering> policy(0, tensor.extent(scaleMode));
+    Kokkos::parallel_for(policy, func);
   }
 
-  //
   // 2. use scales and shifts to normalize tensor data
   transform_slices(tensor, scaleMode, scales, shifts);
 
